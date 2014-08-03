566b88decabc770ef077bc11ad1c324baefdd3f9
vanakenm <martin.vanaken@8thcolor.com>
2014-08-03 17:04:16 +0200

    Clean up UI

--- app/controllers/mail_games_controller.rb
+++ app/controllers/mail_games_controller.rb
@@ -38,7 +38,7 @@ class MailGamesController < ApplicationController
     @saved_game.save
 
     status = @saved_game.status
-    body_contents = "TTTGame-#{id} is #{status} - your move!\n Grid: #{@saved_game.data}\n Play by replying to this mail."
+    body_contents = "TTTGame-#{id} is #{status} - your move!\n Grid: \n #{@saved_game.grid_s}\n Play by replying to this mail."
     
     message = Mail.new do
       from            'martin.vanaken@8thcolor.com'
@@ -66,7 +66,7 @@ class MailGamesController < ApplicationController
     @saved_game.save
 
     id = @saved_game.id
-    body_contents = "TTTGame-#{id} has just started - your move!\n Grid: #{game.save}\n Play by replying to this mail."
+    body_contents = "TTTGame-#{id} has just started - your move!\n Grid: \n #{@saved_game.grid_s}\n Play by replying to this mail."
 
     message = Mail.new do
       from            'martin.vanaken@8thcolor.com'
--- app/models/saved_game.rb
+++ app/models/saved_game.rb
@@ -6,4 +6,16 @@ class SavedGame < ActiveRecord::Base
   def game
     @game ||= Game::load(data)
   end
+
+  def grid_s
+    grid_str = "  0 1 2\n"
+    (0..2).each do |y|
+      grid_str << y.to_s + " " 
+      (0..2).each do |x|
+        grid_str << game.show_at(y,x) + " "
+      end
+      grid_str << "\n"
+    end
+    grid_str
+  end
 end
