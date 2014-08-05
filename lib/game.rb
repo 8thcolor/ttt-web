a2b62776079718e28dc0d930f2d640d44371a7aa
Martin <martin.vanaken@8thcolor.com>
2014-08-05 15:39:03 +0200

    Cleanup code for presentation.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,4 @@
 class Game
-  LINES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-  UI = ['-', 'O', 'X']
 
   def initialize(next_player)
     @grid = [[0,0,0],[0,0,0],[0,0,0]]
@@ -13,6 +11,27 @@ class Game
 
   def toggle_next
     @next *= -1
+  end
+
+  def place(x, y, value = @next)
+    raise "Finished" if finish?
+    raise "Out of board" unless [0, 1, 2].include?(x) && [0, 1, 2].include?(y)
+    raise "Not your turn" if value != @next
+    raise "Cannot play at #{x},#{y} - already taken" unless @grid[x][y] == 0
+    
+    @grid[x][y] = value
+    toggle_next
+  end
+
+  def save
+    data = (@next == 1) ? "O|" : "X|"
+    (0..2).each do |x|
+      (0..2).each do |y|
+          data << show_at(x,y)
+      end
+    end
+    
+    data
   end
 
   def self.load(data)
@@ -35,23 +54,15 @@ class Game
     game
   end
 
+
+  UI = ['-', 'O', 'X']
+
   def show_at(x, y)
     UI[@grid[x][y]]
   end
 
   def to_s
     save
-  end
-
-  def save
-    data = (@next == 1) ? "O|" : "X|"
-    (0..2).each do |x|
-      (0..2).each do |y|
-          data << show_at(x,y)
-      end
-    end
-    
-    data
   end
 
   def next
@@ -82,20 +93,12 @@ class Game
     @next * -1
   end
 
+  LINES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
   def finish?
     LINES.each do |x|
-      return true if (@grid.flatten[x[0]] + @grid.flatten[x[1]] + @grid.flatten[x[2]]).abs == 3
+      return true if (0..2).map { |n| @grid.flatten[x[n]] }.inject(0,:+).abs == 3
     end
     false
-  end
-
-  def place(x, y, value = @next)
-    raise "Finished" if finish?
-    raise "Out of board" unless [0, 1, 2].include?(x) && [0, 1, 2].include?(y)
-    raise "Not your turn" if value != @next
-    raise "Cannot play at #{x},#{y} - already taken" unless @grid[x][y] == 0
-    
-    @grid[x][y] = value
-    toggle_next
   end
 end
